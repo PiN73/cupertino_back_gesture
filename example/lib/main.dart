cd36fadb8cb39fbe5d6b6eba195218dedbd008fd
Pavel <p97p@ya.ru>
2019-10-30 02:27:02 +0300

    implement example app

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -6,6 +6,9 @@ class App extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      theme: ThemeData(
+        platform: TargetPlatform.iOS,
+      ),
       home: MainPage(),
     );
   }
@@ -19,12 +22,32 @@ class MainPage extends StatelessWidget {
         title: Text('Cupertino back gesture'),
       ),
       body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text('Hi'),
-          ],
+        child: RaisedButton(
+          child: Text('Push other page'),
+          onPressed: () {
+            Navigator.of(context).push(
+              MaterialPageRoute(
+                builder: (context) {
+                  return OtherPage();
+                },
+              ),
+            );
+          },
         ),
+      ),
+    );
+  }
+}
+
+class OtherPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Other page'),
+      ),
+      body: Center(
+        child: Text('Try to swipe back'),
       ),
     );
   }
